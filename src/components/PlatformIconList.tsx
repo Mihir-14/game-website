00c9ab289eb996d44d872f1eeb5e947dd4705246
Mihir Dudhatra <mihirdudhatra3906@gmail.com>
2024-08-24 15:19:19 +0530

    Game Grid and list fetch done

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import {
+  FaAndroid,
+  FaApple,
+  FaLinux,
+  FaPlaystation,
+  FaWindows,
+  FaXbox,
+} from "react-icons/fa";
+import { SiNintendo } from "react-icons/si";
+import { MdPhoneIphone } from "react-icons/md";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
+
+interface props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color="gray.500" />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
